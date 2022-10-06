25d7486e78eb45efa075d712067953da47a0bd03
Joseph Lynn <josephlynn94@gmail.com>
2022-10-06 13:24:31 -0400

    feat: adds basic navigation

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from "react";
+import navbarStyles from "../styles/Navbar.module.scss";
+import buttonStyles from "../styles/Button.module.scss";
+
+const Navbar = () => {
+  return (
+    <nav className={navbarStyles.container}>
+      <div className={navbarStyles.logo}>
+        <h3>Platform Launch</h3>
+      </div>
+      <div className={navbarStyles.actions}>
+        <button type="button" className={buttonStyles.primary}>
+          + Add New Task
+        </button>
+        <span className={navbarStyles.actionsIcon}>
+          <img src="./3-dots.svg" alt="" />
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
